990621d81e4fa5661452441c6aa05ff14a03890d
Carlos Gil <dudigil6@gmail.com>
2024-11-05 15:39:22 -0300

    Novos exercicios

--- src/Exercism/FizzBuzz.java
+++ src/Exercism/FizzBuzz.java
@@ -9,18 +9,32 @@ public class FizzBuzz {
 		
 		FizzBuzz fb = new FizzBuzz();
 	
-		fb.fizzBuzz(0);
+		fb.fizzBuzz(6);
 	}
 
 	
     public List<String> fizzBuzz(int n) {
     	List<String> res = new ArrayList<>();
         
-        for(int i = 0; i <= n; i++) {
+        for(int i = 1; i <= n; i++) {
         	Integer index = i;
+        	if(i % 3 == 0 && i % 5 == 0) {
+        		res.add("FizzBuzz");
+        		continue;
+        	}
+        	if(i % 3 == 0) {
+        		res.add("Fizz");
+        		continue;
+        	}
+        	if(i % 5 == 0) {
+        		res.add("Buzz");
+        		continue;
+        	}
+        	index.toString();
         	res.add(index.toString());
+        	
         }
-        return res;
+         return res;
     }
 	
 }
